f9241333c8da18e840b2e48e29f51b6d1e1be090
jurevito <jure.vito@gmail.com>
2023-01-08 18:25:30 +0200

    Put data in same path for Airflow and Spark containers

--- data/job.py
+++ data/job.py
@@ -1,16 +1,19 @@
 from pyspark.sql import SparkSession
 
-# Create a SparkSession
-spark = SparkSession.builder.appName("Transforming JSON data").getOrCreate()
+spark = SparkSession.builder.getOrCreate()
 
 # Read the JSON file and create a dataframe
-df = spark.read.option("multiLine", "true").json("./work/data.json")
+df = spark.read.option("multiLine", "true").json('file:///data/data.json')
 
 # Transform a field in the dataframe
 df = df.withColumn("age", df["age"]*2)
 
+df.printSchema()
+df.show()
+
 # Save the dataframe as an array of JSON objects in a JSON file
-df.write.json("./work/output.json", mode="overwrite")
+single_df = df.coalesce(1)
+single_df.write.json("file:///data/output", mode="overwrite")
 
 # Stop the SparkSession
 spark.stop()
