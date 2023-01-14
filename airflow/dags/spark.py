5de369e3a93ad32380f10d01fe6e7afd88d12be1
jurevito <jure.vito@gmail.com>
2023-01-14 20:02:09 +0200

    Add DAG that parses publication categories

--- airflow/dags/spark.py
+++ airflow/dags/spark.py
@@ -11,6 +11,39 @@ from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOpe
 
 # Other imports.
 from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def get_categories():
+
+    # Fetch HTML for categories.
+    url = 'https://arxiv.org/category_taxonomy'
+    response = requests.get(url)
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+    h4_elements = soup.find_all('h4')
+
+    data = []
+
+    # Extract code with name from each tag.
+    for h4 in h4_elements:
+        if h4.span is not None:
+            
+            name = h4.span.text[1:-1]
+            h4.span.extract()
+            code = h4.text.strip()
+
+            data.append({
+                'code': code,
+                'name': name,
+            })
+
+    # Save it into a JSON file.
+    with open('/data/categories.json', 'w+') as json_file:
+        json.dump(data, json_file)
+
 
 default_args = {
     'depends_on_past': False,
@@ -34,13 +67,23 @@ first_task = BashOperator(
     dag=dag,
 )
 
-# Create Spark Submit task.
+# Fetches information about publication category
+# and saves it into JSON file.
+categories_task = PythonOperator(
+    task_id='categories_task',
+    python_callable=get_categories,
+    dag=dag
+)
+
+# Sumbits a Spark job which transforms data.
 submit_job = SparkSubmitOperator(
-	application ='/data/job.py',
-	conn_id= 'spark_container',
-	task_id='spark_submit_task',
+    application ='/data/job.py',
+    conn_id= 'spark_container',
+    task_id='spark_submit_task',
+    name='airflow-spark',
     verbose=1,
-	dag=dag
+    dag=dag
 )
 
 first_task >> submit_job
+first_task >> categories_task
