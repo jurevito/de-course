baca41bae88eced5fa1919d9886ae2ab45507053
jurevito <jure.vito@gmail.com>
2023-01-15 12:48:33 +0200

    Rename DAG

--- airflow/dags/ingestion.py
+++ airflow/dags/ingestion.py
@@ -8,6 +8,7 @@ from airflow.operators.python_operator import PythonOperator, BranchPythonOperat
 from airflow.operators.bash_operator import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.neo4j.operators.neo4j import Neo4jOperator
 
 # Other imports.
 from datetime import datetime, timedelta
@@ -53,7 +54,7 @@ default_args = {
 
 # Initialize DAG.
 dag = DAG(
-    dag_id='spark',
+    dag_id='ingestion',
     default_args=default_args,
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 1, 1),
@@ -70,20 +71,35 @@ first_task = BashOperator(
 # Fetches information about publication category
 # and saves it into JSON file.
 categories_task = PythonOperator(
-    task_id='categories_task',
+    task_id='fetch_categories',
     python_callable=get_categories,
     dag=dag
 )
 
-# Sumbits a Spark job which transforms data.
+# Submits a Spark job which transforms data.
 submit_job = SparkSubmitOperator(
     application ='/data/job.py',
     conn_id= 'spark_container',
-    task_id='spark_submit_task',
+    task_id='spark_submit',
     name='airflow-spark',
     verbose=1,
     dag=dag
 )
 
-first_task >> submit_job
-first_task >> categories_task
+# Executes a query on Neo4j database.
+neo4j_task = Neo4jOperator(
+    task_id="neo4j_query",
+    neo4j_conn_id="neo4j_container",
+    sql="""
+    CREATE (forrestGump:Movie {title: 'Forrest Gump', released: 1994})
+    CREATE (robert:Person:Director {name: 'Robert Zemeckis', born: 1951})
+    CREATE (tom:Person:Actor {name: 'Tom Hanks', born: 1956})
+    CREATE (tom)-[:ACTED_IN {roles: ['Forrest']}]->(forrestGump)
+    CREATE (robert)-[:DIRECTED]->(forrestGump)
+    """,
+    dag=dag,
+)
+
+# Setup order of execution.
+first_task >> submit_job >> neo4j_task
+first_task >> categories_task >> neo4j_task
